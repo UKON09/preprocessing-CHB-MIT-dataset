451c8526d3f6fff8ef8a239796b7160a9266730c
UKON09 <98503029+UKON09@users.noreply.github.com>
2025-04-07 23:33:18 +0800

    Update preprocess_data.py
    “perf : 完善相关信息”

--- preprocess_data.py
+++ preprocess_data.py
@@ -412,26 +412,3 @@ def preprocess_data(file_dir, save_dir, annotations_df, channels=None):
             print(f'{file_dir}：{file} 数据保存至 {save_path}')
 
     print(f'{file_dir}：完成数据预处理')
-
-
-if __name__ == '__main__':
-    matplotlib.use('TkAgg')
-    plt.ion()
-    # 存在非法电极(空电极)，这里可以不处理，因为pick channels会选择目标电极 drop 不需要的电极
-    invalid_chb = ['chb14', 'chb16', 'chb17', 'chb20', 'chb21', 'chb22']
-    empty_channels = ['--0', '--1', '--2', '--3', '--4']
-
-    # 调用函数时传入路径参数
-    time_table_path = "seizures_info.csv"  # 患者发病发病起止时间表
-    file_dir = "./CHB-MIT_dataset/chb01"  # 数据文件所在目录
-    save_dir = r"E:\data_preprocessing\CHB-MIT\CHB-MIT_pre_dataset"  # 保存的目录
-
-    # 读取发病时间表，结构如下：
-    # file（index）     num       start       end       duration
-    # chb06_01          1         1724       1738        14
-    annotations_df = pd.read_csv(time_table_path, index_col="file")
-
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)  # 如果目录不存在，创建目录
-        print("保存目录不存在，现已建立")
-    preprocess_data(file_dir, save_dir, annotations_df)
